45e34cdf78121a604e6eacd54f7fa67e05369bfd
EhsanDelaramifar <122319111+EhsanDelaramifar@users.noreply.github.com>
2023-01-13 01:25:38 +0330

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,30 @@
+mysol = 0
+my_number = 600851475143
+
+import math
+def is_prime(n):
+    if n % 2 == 0 and n > 2: 
+        return False
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+
+
+def divisorGenerator(n):
+    large_divisors = []
+    for i in range(1, int(math.sqrt(n) + 1)):
+        if n % i == 0:
+            yield i
+            if i*i != n:
+                large_divisors.append(n / i)
+    for divisor in reversed(large_divisors):
+        yield divisor
+
+alist = list(divisorGenerator(my_number))
+
+for a in alist:
+    if is_prime(a):
+        mysol=a
+print(mysol)
